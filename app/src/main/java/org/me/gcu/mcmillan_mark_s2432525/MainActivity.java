b8f576601c51272ac9b5b16f0a0968164aa07471
Mark McMillan <mcmillanmark1@hotmail.com>
2025-10-26 14:54:23 +0000

    init commit

---
+++ app/src/main/java/org/me/gcu/mcmillan_mark_s2432525/MainActivity.java
@@ -0,0 +1,124 @@
+// Name                 Mark McMillan
+// Student ID           S2432525
+// Programme of Study   BSc (Hons) Software Development
+
+package org.me.gcu.mcmillan_mark_s2432525;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private TextView rawDataDisplay;
+    private Button startButton;
+    private String result;
+    private String url1="";
+    private String urlSource="https://www.fx-exchange.com/gbp/rss.xml";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Set up the raw links to the graphical components
+        rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
+        startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+
+        // More Code goes here
+    }
+
+    public void onClick(View aview)
+    {
+        startProgress();
+    }
+
+    public void startProgress()
+    {
+        // Run network access on a separate thread;
+        new Thread(new Task(urlSource)).start();
+    } //
+
+    // Need separate thread to access the internet resource over network
+    // Other neater solutions should be adopted in later iterations.
+    private class Task implements Runnable
+    {
+        private String url;
+        public Task(String aurl){
+            url = aurl;
+        }
+        @Override
+        public void run(){
+            URL aurl;
+            URLConnection yc;
+            BufferedReader in = null;
+            String inputLine = "";
+
+            Log.d("MyTask","in run");
+
+            try
+            {
+                Log.d("MyTask","in try");
+                aurl = new URL(url);
+                yc = aurl.openConnection();
+                in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                while ((inputLine = in.readLine()) != null){
+                    result = result + inputLine;
+                }
+                in.close();
+            }
+            catch (IOException ae) {
+                Log.e("MyTask", "ioexception");
+            }
+
+            //Clean up any leading garbage characters
+            int i = result.indexOf("<?"); //initial tag
+            result = result.substring(i);
+
+            //Clean up any trailing garbage at the end of the file
+            i = result.indexOf("</rss>"); //final tag
+            result = result.substring(0, i + 6);
+
+            // Now that you have the xml data into result, you can parse it
+            try {
+                XmlPullParserFactory factory =
+                        XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(true);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput( new StringReader( result ) );
+
+                // YOUR PARSING HERE!!!
+
+            } catch (XmlPullParserException e) {
+                Log.e("Parsing","EXCEPTION" + e);
+                throw new RuntimeException(e);
+            }
+
+            // Now update the TextView to display raw XML data
+            // Probably not the best way to update TextView
+            // but we are just getting started !
+
+            MainActivity.this.runOnUiThread(new Runnable()
+            {
+                public void run() {
+                    Log.d("UI thread", "I am the UI thread");
+                    rawDataDisplay.setText(result);
+                }
+            });
+        }
+    }
+}
