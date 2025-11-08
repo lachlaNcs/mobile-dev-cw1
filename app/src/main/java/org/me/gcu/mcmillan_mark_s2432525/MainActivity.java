4bec01915bf98ac4aa2bfe91ebc39b89ab684f69
lachlaNcs <mcmillanmark1@hotmail.com>
2025-11-08 12:08:28 +0000

    Added AppBar with dark/light mode toggle.

--- app/src/main/java/org/me/gcu/mcmillan_mark_s2432525/MainActivity.java
+++ app/src/main/java/org/me/gcu/mcmillan_mark_s2432525/MainActivity.java
@@ -4,16 +4,26 @@
 
 package org.me.gcu.mcmillan_mark_s2432525;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.me.gcu.mcmillan_mark_s2432525.model.CurrencyRate;
@@ -32,17 +42,73 @@ import java.net.URLConnection;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String PREF_FILE_NAME = "user_settings";
+    private static final String PREF_THEME_MODE = "theme_mode";
+    private MenuItem themeToggleItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        applySavedTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Set up the raw links to the graphical components
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment_container, new AllRatesFragment())
                     .commit();
+        }
+    }
+
+    private void applySavedTheme() {
+        SharedPreferences prefs = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        int savedMode = prefs.getInt(PREF_THEME_MODE, AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(savedMode);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        themeToggleItem = menu.findItem(R.id.action_toggle_theme);
+        updateThemeIcon();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_toggle_theme) {
+            toggleTheme();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleTheme() {
+        boolean isDarkMode = isDarkMode();
+        int newMode = isDarkMode
+                ? AppCompatDelegate.MODE_NIGHT_NO
+                : AppCompatDelegate.MODE_NIGHT_YES;
+
+        SharedPreferences prefs = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        prefs.edit().putInt(PREF_THEME_MODE, newMode).apply();
+
+        AppCompatDelegate.setDefaultNightMode(newMode);
+    }
+
+    private boolean isDarkMode() {
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+    }
+
+    private void updateThemeIcon() {
+        if (themeToggleItem == null) return;
+
+        if (isDarkMode()) {
+            themeToggleItem.setIcon(R.drawable.baseline_light_mode_24);
+        } else {
+            themeToggleItem.setIcon(R.drawable.baseline_dark_mode_24);
         }
     }
 }
